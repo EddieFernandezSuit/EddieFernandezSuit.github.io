9a2dd1860e8acc788443d501d51d43c012fba0c9
Blastx6 <92227235+Blastx6@users.noreply.github.com>
2023-01-17 23:36:30 -0800

    circles

---
+++ circles/index.js
@@ -0,0 +1,3 @@
+document.getElementById('jesse-button').addEventListener('click', function() {
+    document.getElementById('jesse-circles').innerHTML = document.getElementById('jesse-circles').innerHTML * 1 + 1;
+});
