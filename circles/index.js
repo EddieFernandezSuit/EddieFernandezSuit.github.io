2065ca7baf160a2051cb8ea0193e1706b4f98fa2
Blastx6 <92227235+Blastx6@users.noreply.github.com>
2023-01-18 00:03:30 -0800

    circles

--- circles/index.js
+++ circles/index.js
@@ -1,3 +1,15 @@
-document.getElementById('jesse-button').addEventListener('click', function() {
+document.getElementById('jesse-plus-button').addEventListener('click', function() {
     document.getElementById('jesse-circles').innerHTML = document.getElementById('jesse-circles').innerHTML * 1 + 1;
+});
+
+document.getElementById('jesse-minus-button').addEventListener('click', function() {
+    document.getElementById('jesse-circles').innerHTML = document.getElementById('jesse-circles').innerHTML * 1 - 1;
+});
+
+document.getElementById('azari-plus-button').addEventListener('click', function() {
+    document.getElementById('azari-circles').innerHTML = document.getElementById('azari-circles').innerHTML * 1 + 1;
+});
+
+document.getElementById('azari-minus-button').addEventListener('click', function() {
+    document.getElementById('azari-circles').innerHTML = document.getElementById('azari-circles').innerHTML * 1 - 1;
 });
