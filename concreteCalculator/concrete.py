ac7524fb0fa7dc757482fce7e6de9ed60d29c3f3
EddieFernandezSuit <83356892+EddieFernandezSuit@users.noreply.github.com>
2024-03-13 09:08:37 -0700

    Update concrete.py

--- concreteCalculator/concrete.py
+++ concreteCalculator/concrete.py
@@ -4,54 +4,33 @@ def calculate_volume(walls):
         total_cubic_yards += (walls[i]['length'] - walls[i-1]['width']) * walls[i]['width'] * walls[i]['height']
     return total_cubic_yards
 
-# def calculate_cubic_yards(width, height, lengths):
-#     total_cubic_yards = 0
-#     for i in range(len(walls)):
-#         total_cubic_yards += (lengths[i] - width) * width * height
-#     return total_cubic_yards
-
-def fixedWidthAndHeight():
-    width = int(input('Width: '))
-    height = int(input('Height: '))
-
-    lengths = []
-
-    i = 1
-    length = input('Length ' + str(i) + ': ')
-    while(length != ''):
-        length = int(length)
-        i += 1
-        lengths.append(length)
-        length = input('Length ' + str(i) + ': ')
-
-    cubic_yards = calculate_cubic_yards(width, height, lengths)
-    print(cubic_yards)
-
-def addNewWall(walls):
-    i = len(walls) + 1
-    print('Wall ' + str(i))
+def add_new_wall(walls):
     wall = {
         'length': float(input('Length: ')),
         'width': float(input('Width: ')),
         'height': float(input('Height: ')),
     }
-    if wall['length'] == 0 or wall['height'] == 0 or wall['width'] == 0:
-        return 1
-    
     walls.append(wall)
-    return 0
 
     
-def variableLengthWidthHeight():
+def calculate_volume_all_walls():
     walls = []
-    stop = 0
-    addNewWall(walls)
+    add_new_wall(walls)
 
-    while stop == 0:
-        stop = addNewWall(walls)
+    while True:
+        choice = input("Do you want to add another wall? (yes/no): ")
+        if choice.lower() == "yes":
+            i = len(walls) + 1
+            print('Wall ' + str(i))
+            add_new_wall(walls)
+
+        elif choice.lower() == "no":
+            break
+        else:
+            print("Invalid choice. Please enter 'yes' or 'no'.")
 
     volume = calculate_volume(walls)
     print('Feet: ' + str(volume))
     print('Yards: ' + str(volume * 1/3 * 1/3 * 1/3))
 
-variableLengthWidthHeight()
+calculate_volume_all_walls()
