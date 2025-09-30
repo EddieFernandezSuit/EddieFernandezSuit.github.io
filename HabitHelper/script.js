7c1f8843959f0d80262abc0bb0d2a15ff8d11f2e
EddieFernandezSuit <83356892+EddieFernandezSuit@users.noreply.github.com>
2025-09-30 09:22:42 -0700

    Update script.js

--- HabitHelper/script.js
+++ HabitHelper/script.js
@@ -103,7 +103,7 @@ const start = async () => {
 
     const SECONDS_TO_MINUTES = 60;
     const STRETCH_TIME = 30
-    const ALL_TIMES = 98;
+    const ALL_TIMES = 164;
     const MANUAL = 0;
 
     const tasks = {
