68271e58dedbf3b41fd3e02f88312ec936527b79
EddieFernandezSuit <83356892+EddieFernandezSuit@users.noreply.github.com>
2025-10-01 10:23:09 -0700

    Update script.js

--- HabitHelper/script.js
+++ HabitHelper/script.js
@@ -3,7 +3,6 @@ import { Gist } from './gist.js'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,8 +25,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const analytics = getAnalytics(app);
+const db = getDatabase(app
 
 const habitHelperRef = ref(db, "habit helper");
 
